48249e7a1458b3f6886558f27ce9cd8df5a87248
Aaron Whitfield <aawhitfield@hotmail.com>
2018-07-25 09:18:58 -0700

    added main menu background removed app bar on main menu

--- app/src/main/java/us/binarychaos/academia/HomeScreenActivity.java
+++ app/src/main/java/us/binarychaos/academia/HomeScreenActivity.java
@@ -2,12 +2,16 @@ package us.binarychaos.academia;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
 
 public class HomeScreenActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Remove title bar
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_home_screen);
     }
 }
