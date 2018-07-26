0a46b39c2d875593cea2c25a61abf8069142959f
Aaron Whitfield <aawhitfield@hotmail.com>
2018-07-26 15:01:39 -0700

    added back button to menu

--- app/src/main/java/us/binarychaos/academia/ClassroomInitialActivity.java
+++ app/src/main/java/us/binarychaos/academia/ClassroomInitialActivity.java
@@ -9,5 +9,12 @@ public class ClassroomInitialActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_classroom_initial);
+
+        // create back button in action bar
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
     }
+
+
 }
