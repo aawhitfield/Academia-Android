02e7cd15384f8b5e444646f63c51f0ea0573f9c1
Aaron Whitfield <aawhitfield@hotmail.com>
2018-09-08 22:00:05 -0700

    health bar resizes with color gradient title bar removed to add more screen real estate. need to add back button

--- app/src/main/java/us/binarychaos/academia/ClassroomInitialActivity.java
+++ app/src/main/java/us/binarychaos/academia/ClassroomInitialActivity.java
@@ -1,12 +1,17 @@
 package us.binarychaos.academia;
 
+import android.annotation.SuppressLint;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.CountDownTimer;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.Window;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -20,6 +25,12 @@ public class ClassroomInitialActivity extends AppCompatActivity {
     int minutes = 3;
     int seconds = 1;
     int timeInMiliseconds = (minutes * 60 + seconds + 2) * 1000;
+    int maxHealthBarWidth = 324;
+    int minHealthBarWidth = 0;
+    double lifePercentage = 1.0;
+    double green = 120.0;
+    double healthHue = 120.0;
+
 
     // create count down timer to represent amount of class left in the period
     final CountDownTimer countDownTimer = new CountDownTimer( timeInMiliseconds, 1000)
@@ -89,13 +100,57 @@ public class ClassroomInitialActivity extends AppCompatActivity {
         }
     };
 
+    @SuppressLint("Range")
+    public void updateHealthBarColor()
+    {
+        float lifePercentage = (float) healthBar.getLayoutParams().width / (float) maxHealthBarWidth;
+        float healthHue = (float) (lifePercentage * green);
+        // change health bar color
+        float[] colorFloats = new float[]{healthHue,1,1};
+        Log.i("width",String.valueOf(healthBar.getLayoutParams().width));
+        Log.i("maxHealthBarWidth", String.valueOf((maxHealthBarWidth)));
+        Log.i("lifePercentage",String.valueOf(lifePercentage));
+        Log.i("green",String.valueOf(green));
+        Log.i("healthHue",String.valueOf(healthHue));
+        healthBar.setColorFilter(Color.HSVToColor(colorFloats));
+    }
+
+    public void incrementHealth(View v)
+    {
+        int tempWidth = healthBar.getLayoutParams().width;
+        tempWidth += 20;
+        if(tempWidth > maxHealthBarWidth)
+            tempWidth = maxHealthBarWidth;
+        healthBar.getLayoutParams().width = tempWidth;
+
+        updateHealthBarColor();
+
+    }
+
+    public void decrementHealth(View v)
+    {
+        int tempWidth = healthBar.getLayoutParams().width;
+        tempWidth -= 20;
+        if(tempWidth < minHealthBarWidth)
+            tempWidth = minHealthBarWidth;
+        healthBar.getLayoutParams().width = tempWidth;
+
+        updateHealthBarColor();
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Remove title bar
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_classroom_initial);
 
+/*
         // create back button in action bar
         Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+*/
 
         // initialize screen objects from Design Layout View
         timeLeft = findViewById(R.id.timeLeft);
@@ -105,7 +160,7 @@ public class ClassroomInitialActivity extends AppCompatActivity {
         countDownTimer.start();
 
         // adjust size of health bar
-        healthBar.getLayoutParams().width = 81;
+        healthBar.getLayoutParams().width = maxHealthBarWidth;
 
     }
     // overrides transitions so activity goes back in reverse with the back button and app bar back button
